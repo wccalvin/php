41729f971cf9bbb76ee3c706f6af097edfc8427f
wccalvin <clayton.calvin@gmail.com>
2018-09-07 16:55:42 -0400

    use new functions

--- project/first/index.php
+++ project/first/index.php
@@ -1,11 +1,18 @@
-<h1>Hello from a web Page</h1>
-
-<p>
-<?php
-echo "Hello, World!\n";
-$sum = 10 + 5;
-echo "The total is ", $sum, "\n";
-?>
-</p>
-<p>Some more text here</p>
-<?php echo 'Some more php code here' ?>
+<!doctype html>
+<head>
+  <title>PHP learning</title>
+</head>
+<body>
+  <h1>Hello from a web Page</h1>
+  <p>
+  <?php
+  echo "Hello, World!\n";
+  $name = strtolower(getenv('USERNAME'));
+  echo "Hello $name\n";
+  $sum = 10 + 5;
+  echo "The total is ", $sum, "\n";
+  ?>
+  </p>
+  <p>Some more text here</p>
+  <?php echo 'Some more php code here' ?>
+</body>

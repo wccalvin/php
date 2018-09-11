50c3369a280715629f6939f21b57614daeadffa0
wccalvin <clayton.calvin@gmail.com>
2018-09-11 17:42:59 -0400

    add script location to include files

--- project/second/inc/hello.php
+++ project/second/inc/hello.php
@@ -1,21 +1,3 @@
 <?php
-
-// This is a single line comment
-# This is also a single line comment
-
-// Below is a DocBlock comment
-/*
-    * This is my first php script.
-    *
-    * @author: Clayton William
-*/
-
-echo 'Hello World!'; //displays Hello World!
-
-/*
-This is a multi-line comment.
-line 1
-line 2
-line 3
-*/
+echo "Hello World! from $argv[0]"; //displays Hello World!
 ?>

d1fe26651af362507df75bc26046781653cceabc
wccalvin <clayton.calvin@gmail.com>
2018-09-11 17:13:14 -0400

    include php file example

---
+++ project/second/index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<head>
+  <title>PHP - include script</title>
+</head>
+<body>
+  <h1>Hello from this web Page</h1>
+  <p>Hello from included php:</p>
+  <p><?php include 'inc/hello.php'; ?></p>
+</body>
